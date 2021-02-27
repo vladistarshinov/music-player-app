43298adb3896132ed57ce33143559cbe3dd304a5
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2021-02-27 17:47:14 +0700

    Backend: add all constructors for Schema model

--- backend/src/album/album.service.ts
+++ backend/src/album/album.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Album, AlbumDocument } from "./schema/album.schema";
 
 @Injectable()
 export class AlbumService {
-
+  constructor(@InjectModel(Album.name) private albumModel: Model<AlbumDocument>) {}
 }
