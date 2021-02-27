420e170c757b6de0916fb501881b05d40105ef53
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2021-02-28 00:09:04 +0700

    Frontend: Sidebar menu

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,28 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Index = () => {
   return (
-    <div>
-      Главная страница
-    </div>
+    <>
+      <Navbar />
+      <div className="center">
+        <h1>Добро пожаловать!</h1>
+        <h3>Здесь собраны лучшие треки!</h3>
+      </div>
+
+      <style jsx>
+        {`
+          .center {
+            margin-top: 150px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+        `}
+      </style>
+    </>
   )
 }
 
---
+++ frontend/pages/tracks/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+      Список треков
+    </div>
+  )
+};
+
+export default Index;
