d062edfdc3d7469e743c92f01ce9b1daf64f45df
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2021-04-06 02:35:06 +0700

    Frontend: track details

--- frontend/pages/tracks/[id].tsx
+++ frontend/pages/tracks/[id].tsx
@@ -1,14 +1,17 @@
 import { Button, Grid, TextField } from '@material-ui/core';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/TrackDetail.module.scss';
 import MainLayout from '../../layouts/MainLayout';
 import { IComment, ITrack } from '../../types/tracks';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
 
-const TrackDetail = () => {
-  const track: ITrack = {_id: '1', name: 'Track 1', artist: 'Исполнитель 1', desc: 'Text text text', listens: 0, cover: 'http://localhost:5000/image/5c90134e-f144-40a5-bbad-47fc42b24aa9.jpg', audio: 'http://localhost:5000/audio/0c861925-3831-4790-a59b-c02cb6efdbe3.mp3', comments: []};
+const TrackDetail = ({ serverTrack }) => {
+  // const track: ITrack = {_id: '1', name: 'Track 1', artist: 'Исполнитель 1', desc: 'Text text text', listens: 0, cover: 'http://localhost:5000/image/5c90134e-f144-40a5-bbad-47fc42b24aa9.jpg', audio: 'http://localhost:5000/audio/0c861925-3831-4790-a59b-c02cb6efdbe3.mp3', comments: []};
 
   const router = useRouter();
+  const [track, setTrack] = useState(serverTrack);
 
   return (
     <MainLayout>
@@ -20,7 +23,7 @@ const TrackDetail = () => {
         Назад
       </Button>
       <Grid container className={styles.track_detail__content}>
-        <img src={track.cover} width={200} height={200} />
+        <img src={`http://localhost:5000/${track.cover}`} width={200} height={200} />
         <div className={styles.track_detail__headers}>
           <h1>Название трека - {track.name}</h1>
           <h1>Исполнитель - {track.artist}</h1>
@@ -57,3 +60,12 @@ const TrackDetail = () => {
 }
 
 export default TrackDetail;
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const res = await axios.get(`http://localhost:5000/tracks/${params.id}`);
+  return {
+    props: {
+      serverTrack: res.data
+    }
+  }
+}
